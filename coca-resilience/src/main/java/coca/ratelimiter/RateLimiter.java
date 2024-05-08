b2d8aa63728615772d6989b1f63c401eaa5c51a4
gumingcn <gumingcn@gmail.com>
2024-05-08 14:41:32 +0800

    add the dependencies for testing and refactor moudle

--- coca-resilience/src/main/java/coca/ratelimiter/RateLimiter.java
+++ coca-resilience/src/main/java/coca/ratelimiter/RateLimiter.java
@@ -2,18 +2,66 @@ package coca.ratelimiter;
 
 import coca.core.EventConsumer;
 import coca.core.exception.AcquirePermissionCancelledException;
+import coca.core.funcs.CheckedSupplier;
 import coca.ratelimiter.event.RateLimiterEvent;
 import coca.ratelimiter.event.RateLimiterOnFailureEvent;
 import coca.ratelimiter.event.RateLimiterOnSuccessEvent;
+import coca.ratelimiter.internal.AtomicRateLimiter;
+import io.vavr.control.Either;
 
 import java.time.Duration;
 import java.util.Map;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import static java.util.Collections.emptyMap;
 
 public interface RateLimiter {
     String getName();
     Map<String, String> getTags();
     EventPublisher getEventPublisher();
     RateLimiterConfig getRateLimiterConfig();
+
+    static RateLimiter of(String name, RateLimiterConfig rateLimiterConfig) {
+        return of(name, rateLimiterConfig, emptyMap());
+    }
+
+    static RateLimiter of(String name, RateLimiterConfig rateLimiterConfig,
+                          Map<String, String> tags) {
+        return new AtomicRateLimiter(tags, name, rateLimiterConfig);
+    }
+
+    default <T> T executeSupplier(Supplier<T> supplier) {
+        return executeSupplier(1, supplier);
+    }
+    default <T> T executeSupplier(int permits, Supplier<T> supplier) {
+        return decorateSupplier(this, permits, supplier).get();
+    }
+
+    static <T> Supplier<T> decorateSupplier(RateLimiter rateLimiter, int permits, Supplier<T> supplier) {
+        return decorateCheckedSupplier(rateLimiter, permits, supplier::get)
+                .unchecked();
+    }
+    static <T> CheckedSupplier<T> decorateCheckedSupplier(RateLimiter rateLimiter, int permits,
+                                                          CheckedSupplier<T> supplier) {
+        return ()-> {
+            waitForPermission(rateLimiter,permits);
+            try{
+                T result = supplier.get();
+                rateLimiter.onResult(result);
+                return result;
+            }catch (Exception e) {
+                rateLimiter.onError(e);
+                throw e;
+            }
+        };
+    }
+
+    static <T> Supplier<T> decorateSupplier(RateLimiter rateLimiter, Supplier<T> supplier) {
+        return decorateSupplier(rateLimiter, 1, supplier);
+    }
+
+
     default boolean acquirePermission() {
         return acquirePermission(1);
     }
@@ -35,6 +83,24 @@ public interface RateLimiter {
             throw RequestNotPermitted.createRequestNotPermitted(rateLimiter);
         }
     }
+    default void onError(Throwable throwable) {
+        drainIfNeeded(Either.left(throwable));
+    }
+    default void onSuccess() {
+        drainIfNeeded(Either.right(null));
+    }
+
+    default void onResult(Object result) {
+        drainIfNeeded(Either.right(result));
+    }
+    default void drainIfNeeded(Either<? extends Throwable, ?> callsResult) {
+        Predicate<Either<? extends Throwable, ?>> checker = getRateLimiterConfig()
+                .getDrainPermissionsOnResult();
+        if (checker != null && checker.test(callsResult)) {
+            drainPermissions();
+        }
+    }
+
 
     interface EventPublisher extends coca.core.EventPublisher<RateLimiterEvent> {
         EventPublisher onSuccess(EventConsumer<RateLimiterOnSuccessEvent> consumer);
--- coca-resilience/src/main/java/coca/ratelimiter/RateLimiterConfig.java
+++ coca-resilience/src/main/java/coca/ratelimiter/RateLimiterConfig.java
@@ -1,20 +1,25 @@
 package coca.ratelimiter;
 
+import io.vavr.control.Either;
+
 import java.time.Duration;
+import java.util.function.Predicate;
 
 public class RateLimiterConfig {
     private final int limitForPeriod;
     private final Duration timeoutDuration;
     private final Duration limitRefreshInterval;
     private static final Duration ACCEPTABLE_REFRESH_PERIOD = Duration.ofNanos(1L);
+    private final transient Predicate<Either<? extends Throwable, ?>> drainPermissionsOnResult;
     public static RateLimiterConfig ofDefaults() {
         return new Builder().build();
     }
 
-    public RateLimiterConfig(int limitForPeriod, Duration timeDuration, Duration limitRefreshInterval) {
+    public RateLimiterConfig(int limitForPeriod, Duration timeDuration, Duration limitRefreshInterval, Predicate<Either<? extends Throwable, ?>> drainPermissionsOnResult) {
         this.limitForPeriod = limitForPeriod;
         this.timeoutDuration = timeDuration;
         this.limitRefreshInterval = limitRefreshInterval;
+        this.drainPermissionsOnResult = drainPermissionsOnResult;
     }
 
     public static Builder from(RateLimiterConfig prototype) {
@@ -42,6 +47,7 @@ public class RateLimiterConfig {
         private int limitForPeriod =50;
         private Duration timeoutDuration = Duration.ofSeconds(5L);
         private Duration limitRefreshInterval = Duration.ofNanos(500);
+        private Predicate<Either<? extends Throwable, ?>> drainPermissionsOnResult = any -> false;
 
         public Builder() {
         }
@@ -50,6 +56,7 @@ public class RateLimiterConfig {
             this.limitForPeriod = config.limitForPeriod;
             this.timeoutDuration = config.timeoutDuration;
             this.limitRefreshInterval = config.limitRefreshInterval;
+            this.drainPermissionsOnResult = config.drainPermissionsOnResult;
         }
         public Builder limitForPeriod(int limitForPeriod) {
             this.limitForPeriod = limitForPeriod;
@@ -64,7 +71,10 @@ public class RateLimiterConfig {
             return this;
         }
         public RateLimiterConfig build() {
-            return new RateLimiterConfig(limitForPeriod, timeoutDuration, limitRefreshInterval);
+            return new RateLimiterConfig(limitForPeriod, timeoutDuration, limitRefreshInterval,drainPermissionsOnResult);
         }
+    }
+    public Predicate<Either<? extends Throwable, ?>> getDrainPermissionsOnResult() {
+        return drainPermissionsOnResult;
     }
 }
